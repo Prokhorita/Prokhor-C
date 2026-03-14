6bfb2e4d89f3cf88386015d3c04d5dfe096a44e0
chuliass <chuliass@mail.ru>
2026-03-15 02:48:50 +0300

    Добавлена только папка lab1

--- practic01/testcode5.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main () {
-    float a  = 1.8412244288212;
-    printf("%.15f\n",a);
-    return 0;
-    
-}
--- practic01/testcode9.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main () {
-    int a;
-    scanf("%d", a);
-    printf("n & 1", (a % 1));
-    return 0;
-
-    
-}
